eef746437446eaec73a49cfd0553b7efa363ace0
Suraiya Khan <suraiya.uvic@gmail.com>
2017-07-05 08:23:54 -0700

    Added note on file naming convention

--- transform.py
+++ transform.py
@@ -4,10 +4,25 @@ from jinja2 import Environment, FileSystemLoader, Template
 import os.path
 
 #Call syntax
-#C:\Users\Suraiya\khalegh_linking_latest_2\data-linking>python transform.py  --input "C:\Users\Suraiya\khalegh_linking_latest_2\data-linking\web\linkage\config\openshift\sampleinput\web\deployment.sample.input"   --output django_deployment.yml --template deployment.yaml.tpl --projectroot "C:\Users\Suraiya\khalegh_linking_latest_2\data-linking\web\linkage\config\openshift\template\web"
+#C:\Users\Suraiya\khalegh_linking_latest_2\data-linking>python transform.py
+# --input "C:\Users\Suraiya\khalegh_linking_latest_2\data-linking\web\linkage\config\openshift\sampleinput\web\deployment.sample.input"
+# --output djangodeploymentconfig.yml --template deployment.yaml.tpl 
+#--projectroot "C:\Users\Suraiya\khalegh_linking_latest_2\data-linking\web\linkage\config\openshift\template\web"
+
+
+#To make it difficult for the developer to accidentally commit output configuration
+#files or input; in addition to gitignore we enforced a file naming convention. To be
+#more specific the transform.py script would not run to completion if the input
+#file does not have an extension of .input, and output file is not called .env or
+#does not end in .yaml or .yml and the template file needs to have an extension of tpl.
+#The user can commit the template file. The restriction on the template file extension
+#is there so that someone does not assign .yml extension to a template file (which
+#would require deliberately overriding .yml ignore in the .gitignore file) but this
+#restriction could be removed.
 
 INPUT_FILE_EXTENSION  = '.input'
 OUTPUT_FILE_EXTENSION  = ['.yml', '.yaml']
+#this restriction on template file extension could be removed
 TEMPLATE_FILE_EXTENSION  = '.tpl'
 ENV_FILE = '.env'
 
