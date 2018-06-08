2107b38c7bd835ebd13db5b97ba636d56620567e
Suraiya Khan <suraiya.uvic@gmail.com>
2018-06-08 13:23:35 -0700

    PyPI related setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'LigoLib',
+  packages = [''LigoLib'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'PyPi pkg for Linking Library Ligo-lib',
+  author = 'Suraiya Khan',
+  author_email = 'suraiya.uvic@gmail.com',
+  url = 'https://github.com/NovaVic/ligo-lib', # use the URL to the github repo
+  keywords = ['Linking', 'Deduplication', 'Record Linkage'], # arbitrary keywords
+  install_requires=[
+    "python==3.6", 
+  ],
+  classifiers = [
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+)
