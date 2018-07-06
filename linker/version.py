a2d01b30e926ff9ba9048a3920324c321b5a474b
Suraiya Khan <suraiya.uvic@gmail.com>
2018-07-05 22:26:09 -0700

    added more comment/explanation

--- linker/version.py
+++ linker/version.py
@@ -8,3 +8,5 @@ version=subprocess.check_output(['git', 'describe','--always']).decode('utf-8').
 #build would still happen but due to "on tag" set in deployment section of travis config the push to PyPI 
 #would be prevented if there is no tagged commit 
 #the decode and utf-8 options are there to convert byte to string
+#if we want the build to fail (the call to get version would fail) if there is not tagged commit then 
+#we can remove the  '--always' option in the subprocess.check_output command used above
