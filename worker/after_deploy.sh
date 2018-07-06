c0c05f61069d254605cd13e14748982a9e861422
Suraiya Khan <suraiya.uvic@gmail.com>
2018-07-05 22:09:37 -0700

    directory renamed

---
+++ worker/after_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#to build docker image from PyPI package, tagging it and then pushing it to dockerhub
+echo "$DOCKERHUB_PASSWORD" | docker login https://index.docker.io/v1/ -u "$DOCKERHUB_USERNAME" --password-stdin
+
+docker build  ./worker -t  novavic/ligo_celeryworker:$(git describe --always)
+
+#docker tag ligo_celeryworker novavic/ligo_celeryworker
+docker push novavic/ligo_celeryworker:$(git describe --always)
