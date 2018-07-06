bf4d08c9a9c5a61d81a2b034103944e52683d6b2
Suraiya Khan <suraiya.uvic@gmail.com>
2018-07-06 13:25:01 -0700

    tagging with git tag or hash

--- worker/after_deploy.sh
+++ worker/after_deploy.sh
@@ -2,7 +2,7 @@
 #to build docker image from PyPI package, tagging it and then pushing it to dockerhub
 echo "$DOCKERHUB_PASSWORD" | docker login https://index.docker.io/v1/ -u "$DOCKERHUB_USERNAME" --password-stdin
 
-docker build  ./worker -t  novavic/ligo_celeryworker:$(git describe --always)
+#docker build  ./worker -t  novavic/ligo_celeryworker:$(git describe --always)
 
-#docker tag ligo_celeryworker novavic/ligo_celeryworker
+docker tag ligo_celeryworker novavic/ligo_celeryworker:$(git describe --always)
 docker push novavic/ligo_celeryworker:$(git describe --always)
