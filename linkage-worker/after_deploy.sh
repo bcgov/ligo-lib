b05ec7f4f9f5469e0b092750ed64a85062c370c4
Suraiya Khan <suraiya.uvic@gmail.com>
2018-07-05 22:29:01 -0700

    directories removed as part of renaming

--- linkage-worker/after_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "$DOCKERHUB_PASSWORD" | docker login https://index.docker.io/v1/ -u "$DOCKERHUB_USERNAME" --password-stdin
-
-docker build  ./linkage-worker -t  novavic/ligo_celeryworker
-
-#docker tag ligo_celeryworker novavic/ligo_celeryworker
-docker push novavic/ligo_celeryworker:latest
--- linking_ext/readme.rst
+++
@@ -1,5 +0,0 @@
-BCGOV Linking algorithms extension
-==================================
-
-The **BCGOV Linking algorithms extension** provides the algorithms that can be used by LigoLinker
-framework.
