a9e3c06ad7cb9fde25287002a1c5eff820a9e268
Suraiya Khan <suraiya.uvic@gmail.com>
2018-07-05 22:23:34 -0700

    using tag from tagged commit or git hashas version

--- linker/version.py
+++ linker/version.py
@@ -1,1 +1,10 @@
-version=0.14
+
+import subprocess
+
+version=subprocess.check_output(['git', 'describe','--always']).decode('utf-8').strip()
+
+#we are adding always flag to get git hash  when tag is not present..
+#we want the deployment to PyPI (as well as docker) to fail if there is no tagged commit
+#build would still happen but due to "on tag" set in deployment section of travis config the push to PyPI 
+#would be prevented if there is no tagged commit 
+#the decode and utf-8 options are there to convert byte to string
